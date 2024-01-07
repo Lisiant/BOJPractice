90ce6e7f8ce778dc5ca89458c4f0357b9035d35d
박장우 <79884688+Lisiant@users.noreply.github.com>
2024-01-07 17:45:29 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+INF = int(1e9)
+
+max_val, min_val = -INF, INF
+n = int(input())
+data = list(map(int, input().split()))
+op_counts = list(map(int, input().split()))
+
+def calc(prev, new, op):
+    if op == 0:
+        return prev + new
+    
+    if op == 1:
+        return prev - new
+    
+    if op == 2:
+        return prev * new
+    
+    if op == 3:
+        if prev < 0:
+            return -int(-prev / new)
+        return prev // new            
+        
+    
+def solve(cur, idx):
+    global max_val, min_val
+    
+    if idx == n:
+        max_val, min_val = max(max_val, cur), min(min_val, cur)
+        return
+    
+    for i in range(4):
+        
+        if op_counts[i] > 0:
+            op_counts[i] -= 1 # 연산자 하나 사용
+            val = calc(cur, data[idx], i) # 값 계산
+            solve(val, idx + 1) 
+            op_counts[i] += 1 # 다른 연산자 사용 -> 원복
+        
+
+solve(data[0], 1)
+print(max_val)
+print(min_val)
